b241e1fca0b75f41ac23f058b9426e8103c4dd58
Abderrahmane LYAMANI <lyamani.abderrahmane@gmail.com>
2025-07-24 14:54:10 +0100

    YLM new

--- qa-tests/tests/UI/conftest.py
+++ qa-tests/tests/UI/conftest.py
@@ -1,10 +1,18 @@
 import pytest
 from playwright.sync_api import sync_playwright
 
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="chromium", help="Browser to use (chromium, firefox, webkit)")
+    parser.addoption("--headed", action="store_true", help="Run browser in headed mode")
+
 @pytest.fixture(scope="session")
-def browser():
+def browser(request):
+    browser_name = request.config.getoption("--browser")
+    headed = request.config.getoption("--headed")
+
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser_type = getattr(p, browser_name)
+        browser = browser_type.launch(headless=not headed)
         yield browser
         browser.close()
 
